b618172fcb9d784a53cad09f326cfe6677356165
Arshdeep Kaur <ak2676@cornell.edu>
2026-06-18 18:04:16 -0400

    changed copyright message

--- src/App.tsx
+++ src/App.tsx
@@ -40,7 +40,7 @@ export default function App() {
             <div className="w-4 h-4 bg-purple-500"></div>
           </div>
           <p className="text-lg font-black">
-            © 2026 ARSHDEEP KAUR. built with ❤️ and coffee.
+            © 2026 ARSHDEEP KAUR. built with coffee.
           </p>
         </div>
       </footer>
